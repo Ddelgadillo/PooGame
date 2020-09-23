a73ddd9a19ec07ba114948d16ee24ecd3076ec92
Diego Delgadillo <ddelgadillo11@gmail.com>
2020-09-23 18:53:10 -0500

    Added update class which moves and reverses poo vel

--- Engine/Poo.h
+++ Engine/Poo.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Vec2.h"
 #include "Graphics.h"
+#include "Walls.h"
 
 class Poo
 {
@@ -8,10 +9,12 @@ public:
 	Poo() = default;
 	Poo(Vec2& pos, Vec2& vel);
 	void Draw(Graphics& gfx) const;
+	void Update(float delta);
+	bool WallCollision(Walls& walls);
 private:
 	Vec2 mPos;
 	Vec2 mVel;
-	float mWidth;
-	float mHeight;
+	const float mWidth = 24.0f;
+	const float mHeight = 24.0f;
 	bool mIsEaten;
 };
